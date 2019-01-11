76252225075a36212ca0617ecc7d17225bf572a1
Richard <richard.trow.davis@gmail.com>
2019-01-10 16:25:20 -0800

    Added custom input functionality for pokemon names, created sprites as jquery test

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
 $(() => {
 
-  $('#pokemon-input').on('submit', (event) => {
-    const pokemonInput = $('#pokemon-input').val()
-    console.log(pokemonInput);
-    event.preventDefault()
-  })
+$('#submit_value').on('click', (data) => {
+const pokemonInput = $('#input').val()
+console.log(pokemonInput);
 
-  //   const promise = $.ajax({
-  //     url: "https://pokeapi.co/api/v2/pokemon/"+pokemonInput+"/"
-  //   });
-  //
-  //   promise.then(
-  //     (data) => {
-  //       console.log(data);
-  //       console.log(pokemonInput);
-  //     },
-  //     () => {
-  //       console.log('Bad request');
-  //     }
-  //   );
-  // })
+const promise = $.ajax({
+  url: "https://pokeapi.co/api/v2/pokemon/"+pokemonInput+"/"
+});
 
+promise.then(
+  (data) => {
+    console.log(data);
+    console.log(data.sprites);
+    const pokemonSprite = $('<img>').attr('src', data.sprites.front_default)
+    $('#pokemon_search').append(pokemonSprite)
+  },
+  () => {
+    console.log('Bad request');
+  }
+)
+
+})
 
 })
