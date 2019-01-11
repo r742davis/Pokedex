643cc58a6c906b90a5bace9a2a72e16a7392569b
Richard <richard.trow.davis@gmail.com>
2019-01-10 21:20:42 -0800

    Added functionality that returns types for pokemon but doesn't return error if second type is undefined

--- js/app.js
+++ js/app.js
@@ -1,25 +1,62 @@
 $(() => {
 
+//Website Organization (Divs, Images, Sections)//
+
+
+
+
+
+//////////////
+
+
+
+
 $('#submit_value').on('click', (data) => {
-const pokemonInput = $('#input').val()
-console.log(pokemonInput);
+  const pokemonInput = $('#input').val().toLowerCase()
+  console.log(pokemonInput);
 
-const promise = $.ajax({
-  url: "https://pokeapi.co/api/v2/pokemon/"+pokemonInput+"/"
-});
+  const promise = $.ajax({
+    url: "https://pokeapi.co/api/v2/pokemon/"+pokemonInput+"/"
+  });
 
-promise.then(
-  (data) => {
-    console.log(data);
-    console.log(data.sprites);
-    const pokemonSprite = $('<img>').attr('src', data.sprites.front_default)
-    $('#pokemon_search').append(pokemonSprite)
-  },
-  () => {
-    console.log('Bad request');
-  }
-)
+  promise.then(
+    (data) => {
+      console.log(data);
+      //Create pokemon name
+      //Create pokemon sprites
+      const pokemonSprite = $('<img>')
+        .attr('src', data.sprites.front_default)
+        .addClass('sprite')
+      //create pokemon types
+      const type1 = $('<p>').text(data.types[0].type.name)
 
-})
+      //Some pokemon do not have 2 types and thus return  2nd type as undefined; if not undefined, then list second type
+      if (data.types[1] !== undefined) {
+        console.log('Yasssssss');
+        const type2 = $('<p>').text(data.types[1].type.name)
+        $(type1)
+          .append(type2)
+      } else if (data.types[1] === undefined) {
+        console.log('Boooo bitch');
+      }
+
+      // for(let i = 0; i <= 76; i++){
+      //     const pokemonAbilities = $('<p>').text(data.moves[i].move.name).addClass('sprite')
+      //     $('#pokemon_search').append(pokemonAbilities)
+      // }
+
+      $('#pokemon_search')
+        .append(pokemonSprite)
+        .append(type1)
+
+
+
+    },
+    () => {
+      console.log('Bad request');
+    }
+  )
+
+  })
 
 })
